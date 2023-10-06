b1c269f6e6398d262923cf28d696f9e0315e0bed
Ferhiya Mohammed Abdullahi <fa223ca@student.lnu.se>
2023-10-06 17:21:59 +0200

    started working on calculator app

---
+++ calculator/cal.js
@@ -0,0 +1,15 @@
+//konstant variables
+const numberbtn=document.querySelectorAll('[data-number]');
+const opertionbtn=document.querySelectorAll('[data-operation]');
+const delbtn=document.querySelector('[data-delete]');
+const equalbtn=document.querySelector('[data-equal]');
+const clearDatabtn=document.querySelector('[data-clear]');
+const previousOperandTextElem=document.querySelector('[previous-data]');
+const currentOperandTextElem=document.querySelector('[current-data');
+
+//read data function
+
+
+
+
+
