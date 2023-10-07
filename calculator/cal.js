fee938193aefe0e0c9a305164bb1824024818eca
Ferhiya Mohammed Abdullahi <fa223ca@student.lnu.se>
2023-10-07 18:21:27 +0200

    added more function in js

--- calculator/cal.js
+++ calculator/cal.js
@@ -2,19 +2,28 @@
   const numberButtons = document.querySelectorAll('[data-number]');
   const operationButtons = document.querySelectorAll('[data-operation]');
   const equalsButton = document.querySelector('[data-equals]');
-  const deleteButton = document.querySelector('[ata-delete]');
+  const deleteButton = document.querySelector('[data-delete]');
   const allClearButton = document.querySelector('[data-clear]');
   const previousOperandTextElement = document.querySelector('[previous-data]');
   const currentOperandTextElement = document.querySelector('[current-data]');
-  const calculator = new calculator(previousOperandTextElement, currentOperandTextElement);
-  
+  //const calculator = new calculator(previousOperandTextElement, currentOperandTextElement);
+ 
+
   //functions needed
   function clear(){
-
+    currentOperandTextElement.innerHTML="";
   }
-  function delet(){
 
+  function del() {
+  currentOperandTextElement.innerHTML="";
+   
   }
+
+  
+  
+  
+  
+  
 
   function appendNumber(){
     
@@ -29,6 +38,8 @@
 
    }
 
-   function Display(){
-    
+   function Display(number){
+    const currentOperandTextElement = document.querySelector('[current-data]');
+    const currentOperand = currentOperandTextElement.textContent;
+    currentOperandTextElement.textContent = currentOperand + number;
    }
