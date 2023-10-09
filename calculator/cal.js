2b908a36c9ad08ba34f07c4bd7ea42c15a3cdaea
Ferhiya Mohammed Abdullahi <fa223ca@student.lnu.se>
2023-10-09 14:21:18 +0200

    added more function in js

--- calculator/cal.js
+++ calculator/cal.js
@@ -4,46 +4,89 @@
   const equalsButton = document.querySelector('[data-equals]');
   const deleteButton = document.querySelector('[data-delete]');
   const allClearButton = document.querySelector('[data-clear]');
-  const currentOperandTextElement = document.querySelector('[current-data]');
-  //const calculator = new calculator(previousOperandTextElement, currentOperandTextElement);
- 
+  const currentOperandTextElement = document.querySelector('[display-data]');
+  let previousOperand = ''; //previous input
+  let currentOperand = ''; //current input
+  let currentOperation = null; //current operation
 
   //functions needed
   function clearText(){
-    console.log(currentOperandTextElement);
-    currentOperandTextElement.innerHTML="";
-    
-  }
-
-  function del() {
-  const currentOP=currentOperandTextElement.textContent;
-  const uppdateCurrentOp=currentOP.slice(0,-1);
-  console.log(uppdateCurrentOp);
-  currentOperandTextElement.textContent=uppdateCurrentOp;
+    previousOperand = ''; //previous input
+    currentOperand = ''; 
+    Display();  
    
   }
 
+  function del() {
+    if (currentOperand === '' && currentOperation !== null) {
+        // Om den aktuella operanden är tom och det finns en aktuell operation
+        // tar bort den senaste operationen
+        currentOperation = null;
+      } else {
+        // Annars tar bort det senaste tecknet i den aktuella operanden
+        currentOperand = currentOperand.slice(0, -1);
+      }
+      Display();
   
-  
-  
-  
-  
-
-  function appendNumber(){
-    
   }
 
-   function Operation(){
+  function appendNumber(number){
+    currentOperand=currentOperand+number;
+    Display();
+  }
 
-    
+   function Operation(operation){
+   console.log(operation);
+   if(currentOperand=== "")return;
+   if(previousOperand !== "")
+   {
+    computeRes(); //count the results
+   }
+   currentOperation = operation;
+   previousOperand = currentOperand;
+   currentOperand = '';
+   Display();
    }
 
-   function computeRes(){
+   function computeRes() {
+    let result;
+    const prev = parseFloat(previousOperand);
+    const current = parseFloat(currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (currentOperation) {
+      case '+':
+        result = prev + current;
+        break;
+      case '-':
+        result = prev - current;
+        break;
+      case '*':
+        result = prev * current;
+        break;
+      case '/':
+        result = prev / current;
+        break;
+      default:
+        return;
+    }
+    const expression = prev + ' ' + currentOperation + ' ' + current + ' = ' + result;
+    currentOperand = result.toString();
+    previousOperand = '';
+    currentOperation = null;
+    Display(expression);
+   
+  }
+  
 
-   }
+  function Display(result) {
+    let displayText = previousOperand + ' ' + (currentOperation || '') + ' ';
+    if (result !== undefined) {
+      displayText += result;
+    } else {
+      displayText += currentOperand;
+    }
+    currentOperandTextElement.textContent = displayText;
+  }
 
-   function Display(number){
-    const currentOperandTextElement = document.querySelector('[current-data]');
-    const currentOperand = currentOperandTextElement.textContent;
-    currentOperandTextElement.textContent = currentOperand + number;
-   }
+   
+   
