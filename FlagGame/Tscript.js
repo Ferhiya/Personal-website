412c0b26be92165923a01944fc8ba8adbfcb0f78
Ferhiya Mohammed Abdullahi <fa223ca@student.lnu.se>
2023-11-23 13:11:32 +0100

    added more flags and functionality to flag game

--- FlagGame/Tscript.js
+++ FlagGame/Tscript.js
@@ -10,6 +10,13 @@ const Flags =[
 { name:"france", src: "img/France.jpg" },
 { name:"spain",  src: "img/Spain.jpg"},
 { name:"norway", src: "img/Norway.jpg"},
+{ name:"russia", src: "img/Russia.jpg"},
+{ name:"denmark", src: "img/Denmark.jpg"},
+{ name:"sweden", src: "img/Sweden.jpg"},
+{ name:"finland", src: "img/Finland.jpg"},
+{ name:"italy", src: "img/Italy.jpg"},
+{ name:"germnay", src: "img/Germany.jpg"},
+{ name:"great britan", src:"img/Great Britan.jpg"},
 ]
 
 //Create a function to generate a random flag object from the array.
@@ -30,22 +37,41 @@ function DisplayFlag()
 }
  
 //Add an event listener to the submit button to handle the user's guess.
+let remainingChances = 3;
+
 submitButton.addEventListener("click", function () {
-    const userGuess = guessInput.value;
-    const currentFlag = flagImg.alt;
-    
-    if (userGuess.toLowerCase() === currentFlag.toLowerCase()) {
-      resultMessage.textContent = `Correct! It's ${currentFlag}.`;
-      
+  const userGuess = guessInput.value.toLowerCase();
+  const currentFlag = flagImg.alt.toLowerCase();
+  
+  if (userGuess === currentFlag) {
+    resultMessage.textContent = `Correct! It's ${currentFlag}.`;
+
+    setTimeout(function()
+    {
       DisplayFlag();
-    } else {
-      resultMessage.textContent = `Incorrect. It's ${currentFlag}.`;
+      remainingChances = 3; // Reset chances for the new flag
+    },1000);
+   
+  } else {
+    remainingChances--;
+
+    if (remainingChances > 0) {
+      resultMessage.textContent = `Incorrect. You have ${remainingChances} ${remainingChances === 1 ? 'chance' : 'chances'} left. Try again.`;
       resultMessage.classList.add("message", "incorrect");
+    } else {
+      resultMessage.textContent = `Sorry, you're out of chances. The correct answer is ${currentFlag}.`;
+      resultMessage.classList.add("message", "incorrect");
+      setTimeout(function()
+      {
+        DisplayFlag();
+        remainingChances = 3; // Reset chances for the new flag
+      },2500);
+      
+
     }
-    
-    guessInput.value = "";
-    
-  });;
+  }
+  guessInput.value="";
+});
 
   DisplayFlag();
 
---
+++ Music_player/skript.js
@@ -0,0 +1,16 @@
+"Step 1: Defining all the variables and accessing the HTML elements"
+
+
+
+"Step 2: Loading a new track from the tracklist "
+
+
+"Step 3: Configuring the player buttons "
+
+
+"Step 4: Configuring the sliders portion"
+
+
+
+
+"Step 5: Starting the player"
