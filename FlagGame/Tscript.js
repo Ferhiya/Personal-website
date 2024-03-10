d25d99cc58f7fa900f620968cb4ee86e9c38e106
Ferhiya Mohammed Abdullahi <fa223ca@student.lnu.se>
2024-03-10 13:52:58 +0100

    Update css for flag game

--- FlagGame/Tscript.js
+++ FlagGame/Tscript.js
@@ -1,26 +1,25 @@
 const flagImg = document.getElementById("flag");
 const guessInput = document.getElementById("guess");
-const submitButton = document.getElementById("submit");
 const resultMessage = document.getElementById("result");
 const startButton = document.getElementById("start");
 const timerDisplay = document.getElementById("timer");
 
 // Flag array
 let flags = [
-    { name:"france", src: "img/France.jpg" },
-    { name:"spain",  src: "img/Spain.jpg"},
-    { name:"norway", src: "img/Norway.jpg"},
-    { name:"russia", src: "img/Russia.jpg"},
-    { name:"denmark", src: "img/Denmark.jpg"},
-    { name:"sweden", src: "img/Sweden.jpg"},
-    { name:"finland", src: "img/Finland.jpg"},
-    { name:"italy", src: "img/Italy.png"},
-    { name:"germany", src: "img/Germany.jpg"},
-    { name:"great britan", src:"img/Great-Britian.jpg"},
-    { name:"japan", src:"img/japan.png"},
-    { name:"china", src:"img/china.jpg"},
-    { name:"India", src:"img/india.png"},
-    { name:"saudi arabia", src:"img/Flag_of_Saudi_Arabia.png"}
+    { name: "france", src: "img/France.jpg" },
+    { name: "spain", src: "img/Spain.jpg" },
+    { name: "norway", src: "img/Norway.jpg" },
+    { name: "russia", src: "img/Russia.jpg" },
+    { name: "denmark", src: "img/Denmark.jpg" },
+    { name: "sweden", src: "img/Sweden.jpg" },
+    { name: "finland", src: "img/Finland.jpg" },
+    { name: "italy", src: "img/Italy.png" },
+    { name: "germany", src: "img/Germany.jpg" },
+    { name: "great britan", src: "img/Great-Britian.jpg" },
+    { name: "japan", src: "img/japan.png" },
+    { name: "china", src: "img/china.jpg" },
+    { name: "India", src: "img/india.png" },
+    { name: "saudi arabia", src: "img/Flag_of_Saudi_Arabia.png" }
 ];
 let startTime, endTime, timerInterval; // start timer for game
 let gameStarted = false;
@@ -55,13 +54,8 @@ startButton.addEventListener("click", function () {
     }
 });
 
-// Add an event listener to the submit button to handle the user's guess.
-submitButton.addEventListener("click", function () {
-    submitGuess();
-});
-
 // Listen for Enter key press on the input field
-guessInput.addEventListener("keydown", function(event) {
+guessInput.addEventListener("keydown", function (event) {
     if (event.keyCode === 13) {
         submitGuess();
     }
@@ -75,23 +69,11 @@ function submitGuess() {
         resultMessage.textContent = `Correct! It's ${currentFlag}.`;
         setTimeout(function () {
             displayFlag();
-            remainingChances = 3; // Reset chances for the new flag
         }, 1000);
 
     } else {
-        remainingChances--;
-
-        if (remainingChances > 0) {
-            resultMessage.textContent = `Incorrect. You have ${remainingChances} ${remainingChances === 1 ? 'chance' : 'chances'} left. Try again.`;
-            resultMessage.classList.add("message", "incorrect");
-        } else {
-            resultMessage.textContent = `Sorry, you're out of chances. The correct answer is ${currentFlag}.`;
-            resultMessage.classList.add("message", "incorrect");
-            setTimeout(function () {
-                displayFlag();
-                remainingChances = 3; // Reset chances for the new flag
-            }, 2500);
-        }
+        resultMessage.textContent = `Incorrect. Try again.`;
+        resultMessage.classList.add("message", "incorrect");
     }
     guessInput.value = "";
 }
